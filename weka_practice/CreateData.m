d2e6a78fc858fc76a817a6d13a0e90616c9d2fb9
mohammad <mohammad.h.komeili.1377@gmail.com>
2020-04-29 18:41:09 +0430

    matlab code for creating dataset added

---
+++ weka_practice/CreateData.m
@@ -0,0 +1,11 @@
+a=-5;
+b=5;
+matrix=[];
+ for i=1 : 6000
+x1= a + (b-a)* rand (1);
+x2= a + (b-a)* rand (1);
+class=sign (-2+x1+2 * x2);
+matrix=[matrix; x1 x2 class];
+
+ end
+ save -ascii data.dat matrix
