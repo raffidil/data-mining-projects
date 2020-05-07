b982a4a0f9f26da2c4698f60026ffd0d1b2857e6
mohammad <mohammad.h.komeili.1377@gmail.com>
2020-05-07 22:09:24 +0430

    bug fixed. datasets are fixed now.

--- weka_practice/dataset.py
+++ weka_practice/dataset.py
@@ -5,10 +5,10 @@ attribute xtwo real
 attribute output {-1,1}
 data"""
 file_path = '/home/mohammad/GIT projects/GitHub/data-mining-projects/weka_practice/dataset.arff'
-dataset = np.loadtxt(file_path, delimiter = ',', comments='@' )
-different_sizes = [50, 100, 250, 1650, 2550]
+dataset = np.loadtxt(file_path, delimiter = ',', comments='@' ,dtype= object)
+different_sizes = [50, 100, 250, 550, 1100, 1650, 2550]
 for size in different_sizes:
-    index_array = np.random.randint(low = 0,high = 6000,size = size , dtype="int16")
+    index_array = np.random.randint(low = 0,high = 6000,size = size)
     chosen_data = dataset[index_array,:]
     np.savetxt('/home/mohammad/GIT projects/GitHub/data-mining-projects/weka_practice/randomlyChosen%s'%str(size)+'.arff',
-                chosen_data,delimiter=',',header=header_sting, comments='@')
+                 chosen_data,delimiter=',',header=header_sting, comments='@', fmt='%s')
